a21d511f3b0cee7b7be6f79374e7d66a95e6e43c
Andrei Shkarin <andrei.shkarin@gmail.com>
2019-04-19 21:02:49 +0300

    Sort the values.

--- project/myapp/db/getters.py
+++ project/myapp/db/getters.py
@@ -4,22 +4,28 @@ from myapp.model.address import Address
 
 
 def get_dist_builings_by_zip(zip_code=None):
+    dist = None
     if zip_code is not None:
-        return database.session.query(Address.plz, sa.func.count(Address.object_id))\
+        dist = database.session.query(Address.plz, sa.func.count(Address.object_id))\
                       .filter(Address.plz == zip_code)\
                       .group_by(Address.plz).all()
     else:
-        return database.session.query(Address.plz, sa.func.count(Address.object_id))\
+        dist = database.session.query(Address.plz, sa.func.count(Address.object_id))\
                          .group_by(Address.plz).all()
+
+    return sorted(dist, key=lambda x: int(x[0]))
 
 
 def get_dist_builings_by_years(zip_code=None):
+    dist = None
     if zip_code is not None:
-        return database.session.query(sa.func.extract('year', sa.cast(Address.str_datum, sa.Date)), 
+        dist = database.session.query(sa.func.extract('year', sa.cast(Address.str_datum, sa.Date)),
                              sa.func.count(Address.object_id))\
                       .filter(Address.plz == zip_code)\
                       .group_by(sa.func.extract('year', sa.cast(Address.str_datum, sa.Date))).all()
     else:
-        return database.session.query(sa.func.extract('year', sa.cast(Address.str_datum, sa.Date)), 
+        dist = database.session.query(sa.func.extract('year', sa.cast(Address.str_datum, sa.Date)),
                              sa.func.count(Address.object_id))\
-                      .group_by(sa.func.extract('year', sa.cast(Address.str_datum, sa.Date))).all() 
+                      .group_by(sa.func.extract('year', sa.cast(Address.str_datum, sa.Date))).all()
+
+    return sorted(dist, key=lambda x: int(x[0]))
